d3d1325a406f0005986d1f687fe69d2acbcc2a15
Frankie Bell <fbell.pro@gmail.com>
2016-11-28 13:00:35 +0000

    added in test to check for key value pair

--- lib/dbapp.rb
+++ lib/dbapp.rb
@@ -1,10 +1,16 @@
 require 'sinatra/base'
 
 class DatabaseTT < Sinatra::Base
-  set port:, 4000
+  set port: 4000
 
   get '/' do
     'Hello DatabaseTT!'
+  end
+
+  get '/set' do
+    @string_to_get = request.query_string
+    puts @string_to_get
+    erb :set
   end
 
   # start the server if ruby file executed directly
--- spec/dbapp_spec.rb
+++ spec/dbapp_spec.rb
@@ -1,12 +1,16 @@
-require 'spec_helper'
-
 describe DatabaseTT do
 
   describe 'get /' do
-   it 'displays the home page with log in form' do
-     get '/'
-     expect(last_response).to be_ok
-     expect(last_response.body).to include "Log in to Makers BnB"
+   it 'displays the home page' do
+     visit '/'
+     expect(page).to have_content "Hello DatabaseTT!"
    end
-   
+ end
+
+ describe 'set /' do
+   it 'allows user to set key value pair' do
+     visit '/set?key=value'
+     expect(page).to have_content '{"key"=>"value"}'
+   end
+ end
 end
